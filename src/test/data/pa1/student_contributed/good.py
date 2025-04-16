7b3565617f9beea7748d710ff3999298d4aa0374
Victor Filgueira <victor.filgueira@mindsatwork.com.br>
2025-04-15 21:46:19 -0300

    test: add valid and invalid test cases

--- src/test/data/pa1/student_contributed/good.py
+++ src/test/data/pa1/student_contributed/good.py
@@ -1,1 +1,28 @@
+# Expressões aritméticas válidas
 1 + 2 + 3
+4 * 5 - 6
+7 // 2
+-10
+
+# Atribuições válidas
+x = 10
+y = x + 5
+z = x * y
+
+# Estruturas de controle
+if x > 0:
+    print(x)
+
+while i < 10:
+    i = i + 1
+
+for i in [1, 2, 3]:
+    print(i)
+
+# Chamadas de função
+print("Hello")
+soma(1, 2)
+
+# Listas e indexação
+lista = [1, 2, 3]
+elemento = lista[0]
